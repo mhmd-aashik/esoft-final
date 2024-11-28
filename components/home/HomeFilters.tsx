03123c83e2a3ec2d4d338f0e9a68f568676e1adc
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-28 13:18:43 +0530

    color changes

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -38,7 +38,7 @@ const HomeFilters = () => {
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value ? "bg-primary-500/20 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400" : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"}`}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value ? "bg-primary-500/20 text-primary-500  dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400" : "bg-primary-500/10 text-light-500 hover:bg-primary-500/20 hover:text-primary-500 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"}`}
           onClick={() => handleTypeClick(item.value)}
         >
           {item.name}
